96d72c5a371569de8888559ccb97206c1afb9bb8
Aster <aster@vers.site>
2014-08-27 10:25:56 +0000

    🐥 Define the Group Error types

--- projects/groups-core/src/errors.rs
+++ projects/groups-core/src/errors.rs
@@ -1,6 +1,27 @@
-#[derive(Debug, Copy, Clone)]
-pub enum Error {
-    UnknownError
+pub type Result<T> = std::result::Result<T, GroupError>;
+
+#[derive(Debug)]
+pub struct GroupError {
+    kind: GroupErrorKind,
 }
 
-pub type Result<T> = std::result::Result<T, Error>;
+#[derive(Debug)]
+pub enum GroupErrorKind {
+    InvalidPermutation,
+}
+
+impl GroupError {
+    pub fn invalid_permutation() -> Self {
+        Self {
+            kind: GroupErrorKind::InvalidPermutation,
+        }
+    }
+}
+
+impl GroupError {
+
+
+    pub fn kind(&self) -> &GroupErrorKind {
+        &self.kind
+    }
+}
--- projects/groups-core/src/lib.rs
+++ projects/groups-core/src/lib.rs
@@ -5,5 +5,6 @@
 #![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
 
 mod errors;
+mod cycles;
 
-pub use crate::errors::{Error, Result};
+pub use crate::errors::{GroupError, GroupErrorKind, Result};
